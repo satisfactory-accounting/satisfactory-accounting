9717cfc75749f15f1626fa03477319f0c20e90bf
Zachary Stewart <zachary.c.stewart@gmail.com>
2022-01-10 18:30:32 -0500

    add license

--- satisfactory-accounting-app/src/node_display/building/recipe.rs
+++ satisfactory-accounting-app/src/node_display/building/recipe.rs
@@ -1,3 +1,10 @@
+// Copyright 2021 Zachary Stewart
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
 use log::warn;
 use satisfactory_accounting::database::{BuildingId, BuildingKind, RecipeId};
 use yew::prelude::*;
