b1eb341c3f73d8bb701a3453005235dec2ec3d44
Zachary Stewart <zachary.c.stewart@gmail.com>
2024-10-01 00:04:48 -0400

    finish migrating the menu bar into its own component

--- satisfactory-accounting-app/src/appheader/menubar/mod.rs
+++ satisfactory-accounting-app/src/appheader/menubar/mod.rs
@@ -1,20 +1,25 @@
-use yew::{function_component, html, Callback, Html, Properties};
+use yew::{function_component, html, Html, Properties};
 
-#[derive(Debug, PartialEq, Properties, Clone)]
+#[derive(Debug, PartialEq, Properties)]
 pub struct Props {
-    /// Callback to perform undo, if undo is available.
-    undo: Option<Callback<()>>,
-    /// Callback to perform redo, if redo is available.
-    redo: Option<Callback<()>>,
+    /// Left items in the menu.
+    #[prop_or_default]
+    pub left: Html,
+    /// Right items in the menu.
+    #[prop_or_default]
+    pub right: Html,
 }
 
 /// Displays the main Yew menu bar.
 #[function_component]
-pub fn MenuBar(Props { undo, redo }: &Props) -> Html {
+pub fn MenuBar(Props { left, right }: &Props) -> Html {
     html! {
         <div class="MenuBar">
             <div class="flex-section">
-
+                { left.clone() }
+            </div>
+            <div class="flex-section">
+                { right.clone() }
             </div>
         </div>
     }
--- satisfactory-accounting-app/src/inputs/button/mod.rs
+++ satisfactory-accounting-app/src/inputs/button/mod.rs
@@ -1,18 +1,22 @@
-use yew::{classes, function_component, html, use_callback, Callback, Classes, Html, Properties};
+use yew::{classes, function_component, html, use_callback, AttrValue, Callback, Classes, Html, Properties};
 
 #[derive(Debug, PartialEq, Properties)]
 pub struct Props {
     /// Contents of the button.
     #[prop_or_default]
-    children: Html,
+    pub children: Html,
 
     /// Callback to activate when the button is clicked.
     #[prop_or_default]
-    onclick: Option<Callback<()>>,
+    pub onclick: Option<Callback<()>>,
 
     /// Extra classes to apply to the button.
     #[prop_or_default]
-    class: Classes,
+    pub class: Classes,
+
+    /// Title to set on the main button element.
+    #[prop_or_default]
+    pub title: Option<AttrValue>,
 }
 
 /// Simple button with helper for material icons.
@@ -22,6 +26,7 @@ pub fn Button(
         children,
         onclick,
         class,
+        title,
     }: &Props,
 ) -> Html {
     let disabled = onclick.is_none();
@@ -36,8 +41,54 @@ pub fn Button(
     });
 
     html! {
-        <button {class} {onclick} {disabled}>
+        <button {class} {onclick} {disabled} {title}>
             { children.clone() }
         </button>
+    }
+}
+
+#[derive(Debug, PartialEq, Properties)]
+pub struct LinkProps {
+    /// Contents of the button.
+    #[prop_or_default]
+    pub children: Html,
+
+    /// Callback to activate when the button is clicked.
+    #[prop_or_default]
+    pub href: Option<AttrValue>,
+
+    /// Extra classes to apply to the button.
+    #[prop_or_default]
+    pub class: Classes,
+
+    /// Title to set on the main button element.
+    #[prop_or_default]
+    pub title: Option<AttrValue>,
+
+    /// Html target property for the link.
+    #[prop_or_default]
+    pub target: Option<AttrValue>,
+}
+
+/// Simple button with helper for material icons.
+#[function_component]
+pub fn LinkButton(
+    LinkProps {
+        children,
+        href,
+        class,
+        title,
+        target,
+    }: &LinkProps,
+) -> Html {
+    let class = classes!(
+        "Button",
+        class.clone()
+    );
+
+    html! {
+        <a {class} {href} {target} {title}>
+            { children.clone() }
+        </a>
     }
 }
